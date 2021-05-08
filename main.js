1d515b7df1c6692f0d6582be6b2dadbac5316ca0
Tim Glover <Lstaylor338@Gmail.com>
2021-05-08 11:08:38 -0400

    cleaned up my code and made some adjusments

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var time = 3000;
 images[0] = 'images/book.jpg';
 images[1] = "images/iv.webp";
 images[2] = 'images/math.webp';
-
+images[3] = "images/pills.webp";
 
 function changeImg() {
     document.slide.src = images[i];
